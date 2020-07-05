c885a69f686bf94ab55dd617eb239260d8d3ffba
NikoZhang <593177876@qq.com>
2020-07-05 23:31:11 +0800

    feat: init webpack

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+// import Hello from "./components/Hello";
+
+ReactDOM.render(<h1>adf</h1>, app);
+
+if (module.hot) {
+    module.hot.accept(); // 当入口文件变化后重新执行当前入口文件
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: __dirname + "/dist"
