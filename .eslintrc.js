39e3cf6bee60d1bcb14e449a3ef970ddb0037c93
NikoZhang <593177876@qq.com>
2020-09-20 01:01:26 +0800

    feat: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,37 @@
+/*
+http://eslint.cn/docs/user-guide/getting-started
+https://prettier.io/docs/en/install.html
+https://github.com/prettier/eslint-plugin-prettier
+https://github.com/prettier/eslint-config-prettier#special-rules
+*/
+/*
 module.exports = {
   extends: ["eslint-config-ali", "prettier", "plugin:prettier/recommended"],
   parser: "babel-eslint",
   rules: {
     "prettier/prettier": "error",
-    strict: "off",
-    "no-console": "off",
-    "import/no-dynamic-require": "off",
-    "global-require": "off",
-    "require-yield": "off",
   },
   plugins: ["prettier"],
   globals: {
     React: "readable",
   },
+};
+*/
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
 };
---
+++ src/modules/Demo/Context.tsx
@@ -0,0 +1,36 @@
+/*
+https://www.cnblogs.com/Wayou/p/react_typescript_forwardref.html
+*/
+
+import React, {
+  useState,
+  useRef,
+  createRef,
+  forwardRef,
+  InputHTMLAttributes,
+} from "react";
+
+const FancyInput = React.forwardRef<HTMLInputElement, any>((props, ref) => {
+  return <input type="text" ref={ref} className="fancy-input" {...props} />;
+});
+
+export default function Ref() {
+  const [inputRef, setInputRef] = useState(createRef<HTMLInputElement>());
+
+  return (
+    <section>
+      <FancyInput ref={inputRef} placeholder="abc" />
+      <hr />
+      <button
+        style={{ display: "inline-block" }}
+        onClick={() => {
+          if (inputRef.current) {
+            console.log(inputRef.current.value);
+          }
+        }}
+      >
+        按钮
+      </button>
+    </section>
+  );
+}
