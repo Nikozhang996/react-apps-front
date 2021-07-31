242756eae395fc74c269a913908d01fcd58ffa2e
NikoZhang <593177876@qq.com>
2021-08-01 01:09:52 +0800

    feat: hook

---
+++ src/components/child-component/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC, Props, useMemo, useState} from "react";
+
+export interface IProps extends Props<FC>{
+  value: number;
+}
+
+const ChildComponent = (props: IProps): JSX.Element => {
+  console.log(props);
+
+  return <section>{props.children}:{props.value}</section>;
+};
+
+export default ChildComponent;
--- src/modules/Demo/hook.tsx
+++ src/modules/Demo/hook.tsx
@@ -1,40 +1,50 @@
 /* 
 https://www.cnblogs.com/Wayou/p/react_typescript_forwardref.html
 */
-import React, { useState, useRef, createRef, forwardRef, InputHTMLAttributes } from "react";
+import React, { useState, useMemo, useRef, createRef, forwardRef, InputHTMLAttributes, FC } from "react";
+
+import ChildComponent from "../../components/child-component";
 
 const FancyInput = React.forwardRef<HTMLInputElement, any>((props, ref) => {
   return <input type="text" ref={ref} className="fancy-input" {...props} />;
 });
 
-export default function Ref() {
-  const [inputRef, setInputRef] = useState(createRef<HTMLInputElement>());
-
+const Ref: FC = () => {
+  // const [inputRef, setInputRef] = useState(createRef<HTMLInputElement>());
+  const [value, setValue] = useState<number>(0);
+  const [childValue, setChildValue] = useState<number>(0);
+  const Child = useMemo(() => {
+    return ChildComponent;
+  }, []);
+  console.log(value);
   return (
     <section>
-      <FancyInput ref={inputRef} placeholder="abc" />
+      <ChildComponent value={10}>子组件</ChildComponent>
+      {/*{useMemo(<ChildComponent value={childValue}/>, [childValue])}*/}
+      {/*<FancyInput ref={inputRef} placeholder="abc" />*/}
+      <span>{value}</span>
       <hr />
       <button
         style={{ display: "inline-block" }}
         onClick={() => {
-          if (inputRef.current) {
-            console.log(inputRef.current.value);
-          }
+          setValue((value) => value + 1);
+          setChildValue((value) => value + 1);
         }}
       >
-        按钮
+        加一
       </button>
 
       <button
         style={{ display: "inline-block" }}
         onClick={() => {
-          if (inputRef.current) {
-            console.log(inputRef.current.value);
-          }
+          setValue((value) => value - 1);
+          setChildValue((value) => value - 1);
         }}
       >
-        按钮
+        减一
       </button>
     </section>
   );
-}
+};
+
+export default Ref;
